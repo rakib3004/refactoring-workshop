d1f08ffc5e977ef012ebf2d1aa220fcd1904a970
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-03 02:46:31 +0600

    update trivial  folder

---
+++ java/src/main/java/workshop/TriviaGameModule/PopQuestionPatternMatcher.java
@@ -0,0 +1,13 @@
+package workshop.TriviaGameModule;
+
+public class PopQuestionPatternMatcher implements QuestionPatternMatcher {
+    @Override
+    public boolean match(int number) {
+        return number == 0 || number == 4 || number == 8;
+    }
+
+    @Override
+    public String generateResponse() {
+        return "Pop";
+    }
+}
---
+++ java/src/main/java/workshop/TriviaGameModule/Question.java
@@ -0,0 +1,29 @@
+package workshop.TriviaGameModule;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    private ArrayList<String> question = new ArrayList<>();
+    private String question_type;
+
+    public Question(String question_type) {
+        this.question_type = question_type;
+        generateQuestion();
+    }
+
+    private void generateQuestion() {
+        for (int i = 0; i < 50; i++) {
+            question.add(this.question_type + " Question " + i);
+        }
+    }
+
+    public String getQuestion_type() {
+        return question_type;
+    }
+
+    String getQuestion() {
+        return question.remove(0);
+    }
+}
